3dc0c70c1b1ea4cd55cf4fb81ed4e16692694e38
josesdio <josesdio948@gmail.com>
2023-11-30 17:12:48 +0700

    asfsaf

--- Toys4HI/SignUpViewController.swift
+++ Toys4HI/SignUpViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import CoreData
 
 class SignUpViewController: UIViewController {
 
+    var context: NSManagedObjectContext!
+    var userArray: [String] = []
+    
     @IBOutlet weak var EmailTextField: UITextField!
     
     @IBOutlet weak var PasswordTextField: UITextField!
@@ -17,6 +21,11 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        context = appDelegate.persistentContainer.viewContext
     }
     
     
@@ -27,14 +36,58 @@ class SignUpViewController: UIViewController {
         
         if(password != confirmPassword){
             showAlert(title: "Password is Wrong!", message: "Password does not match!")
+            return
         }
         
         if(!email.hasSuffix("@gmail.com")){
             showAlert(title: "Email is Wrong!", message: "Email must have @gmail.com in it")
+            return
+        }
+        
+        let entityTarget = NSEntityDescription.entity(forEntityName: "User", in: context)
+        
+        if(entityTarget != nil){
+            let newUser = NSManagedObject(entity: entityTarget!, insertInto: context)
+            newUser.setValue(email, forKey: "email")
+            newUser.setValue(password, forKey: "password")
+        }
+        
+        do{
+            try context.save()
+            print("Save success!")
+        }catch{
+            print("Error!")
         }
         
     }
 
+    func fetchUser(){
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        
+        do{
+            var results = try context.fetch(request) as! [NSManagedObject]
+            for data in results{
+                userArray.append(data.value(forKey: "email") as! String)
+            }
+            print("Fetching Success!")
+        }catch{
+            print("Fetching Failed!")
+        }
+    }
+    
+    func isUserExist(email: String){
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        
+        do{
+            var results = try context.fetch(request) as! [NSManagedObject]
+            for data in results{
+                
+            }
+        }catch{
+            
+        }
+    }
+    
     func showAlert(title: String, message: String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
