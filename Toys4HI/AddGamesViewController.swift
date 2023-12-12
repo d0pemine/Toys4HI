7a6243ae03ff173dae93e8aa25b0b7281e1d2bbd
Mario Indra Radityaputra <116057711+d0pemine@users.noreply.github.com>
2023-12-12 13:43:40 +0700

    Update and rename AddGameViewController.swift to AddGamesViewController.swift

--- Toys4HI/AddGamesViewController.swift
+++ Toys4HI/AddGamesViewController.swift
@@ -8,13 +8,8 @@
 import UIKit
 import CoreData
 
-class AddGameViewController: UIViewController {
+class AddGamesViewController: UIViewController {
 
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var descriptionTextField: UITextField!
-    @IBOutlet weak var categoryTextField: UITextField!
-    @IBOutlet weak var priceTextField: UITextField!
-    
     var context: NSManagedObjectContext!
     
     override func viewDidLoad() {
@@ -24,43 +19,51 @@ class AddGameViewController: UIViewController {
         context = appDelegate.persistentContainer.viewContext
     }
     
-    @IBAction func addButton(_ sender: Any) {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descTextField: UITextField!
+    @IBOutlet weak var CategoryTextField: UITextField!
+    @IBOutlet weak var PriceTextField: UITextField!
+    
+    
+    @IBAction func addBtn(_ sender: Any) {
         guard let gameName = nameTextField.text, !gameName.isEmpty else {
             showAlert(title: "Game Name is Empty!", message: "Name must be filled")
             return
         }
-        
-        guard let gameDescription = descriptionTextField.text, !gameDescription.isEmpty else{
+                
+        guard let gameDescription = descTextField.text, !gameDescription.isEmpty else{
             showAlert(title: "Game Description is Empty", message: "Description must be filled")
             return
         }
-        
-        guard let gameCategory = categoryTextField.text, !gameCategory.isEmpty else{
+                
+        guard let gameCategory = CategoryTextField.text, !gameCategory.isEmpty else{
             showAlert(title: "Game Category is Empty", message: "Category must be filled")
             return
-        }
-        
-        guard let gamePrice = priceTextField.text, !gamePrice.isEmpty else{
+            }
+                
+        guard let gamePrice = PriceTextField.text, !gamePrice.isEmpty else{
             showAlert(title: "Game Price is Free???", message: "Really???")
             return
         }
-        
-        let entityTarget = NSEntityDescription.entity(forEntityName: "Game", in: context)
-        
+                
+        let entityTarget = NSEntityDescription.entity(forEntityName: "Games", in: context)
+                
         if(entityTarget != nil) {
             let newGame = NSManagedObject(entity: entityTarget!, insertInto: context)
-            
+                    
             newGame.setValue(gameName, forKey: "gameName")
             newGame.setValue(gameCategory, forKey: "gameCategory")
             newGame.setValue(gameDescription, forKey: "gameDescription")
             newGame.setValue(Int(gamePrice), forKey: "gamePrice")
         }
-        
+                
         do{
             try context.save()
         }catch{
             print("Error Saving Game!")
         }
+        
     }
     
     
